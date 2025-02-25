e8559e0ec594bd880c6d6ff0ab650f5c0d9ea429
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2025-02-25 21:15:17 +0530

    Time: 0 ms (100.00%) | Memory: 112 MB (80.00%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     int numOfSubarrays(vector<int>& arr) {
-        int odd_count = 0, even_count = 1; // even_count starts as 1 (for prefix sum = 0)
-        int prefix_sum = 0, mod = 1e9 + 7;
-        int count = 0;
+        // int odd_count = 0, even_count = 1; 
+        // int prefix_sum = 0, mod = 1e9 + 7;
+        // int count = 0;
 
-        for (int num : arr) {
-            prefix_sum += num;
-            if (prefix_sum % 2 == 0) {
-                count = (count + odd_count) % mod;
+        // for (int num : arr) {
+        //     prefix_sum += num;
+        //     if (prefix_sum % 2 == 0) {
+        //         count = (count + odd_count) % mod;
+        //         even_count++;
+        //     } else {
+        //         count = (count + even_count) % mod;
+        //         odd_count++;
+        //     }
+        // }
+        int odd_count=0,even_count=1;
+        int prefix_sum=0,count=0;
+        int mod=1e9+7;
+        for(int num:arr){
+            prefix_sum+=num;
+            if(prefix_sum%2==0){
+                count=(count+odd_count)%mod;
                 even_count++;
-            } else {
-                count = (count + even_count) % mod;
+            }else{
+                count=(count+even_count)%mod;
                 odd_count++;
             }
         }
