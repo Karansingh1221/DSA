cdb07b6db73267c963238a1d72e025cf6d29e2b4
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2025-02-25 18:51:19 +0530

    Time: 628 ms (5.03%) | Memory: 51.7 MB (10.04%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid,vector<vector<bool>>& visited,int i,int j){
+    void dfs(vector<vector<int>>& grid,int i,int j){
         int m=grid.size();
         int n=grid[0].size();
-        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]!=1 ||visited[i][j]){
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]!=1){
             return;
         }
-        visited[i][j]=true;
+        grid[i][j]=-1;
         vector<pair<int,int>> dirs={{1,0},{0,1},{-1,0},{0,-1}};
         for(auto [x,y] : dirs){
-            dfs(grid,visited,x+i,y+j);
+            dfs(grid,x+i,y+j);
         }
     }
     int numEnclaves(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<bool>> visited(m,vector<bool>(n,false));
+        // vector<vector<bool>> visited(m,vector<bool>(n,false));
         for(int i=0;i<m;i++){
-            if(grid[i][0]==1 && !visited[i][0]){
-                dfs(grid,visited,i,0);
+            if(grid[i][0]==1 ){
+                dfs(grid,i,0);
             }
-            if(grid[i][n-1]==1 && !visited[i][n-1]){
-                dfs(grid,visited,i,n-1);
+            if(grid[i][n-1]==1){
+                dfs(grid,i,n-1);
             }
         }
         for(int j=0;j<n;j++){
-            if(grid[0][j]==1 && !visited[0][j]){
-                dfs(grid,visited,0,j);
+            if(grid[0][j]==1){
+                dfs(grid,0,j);
             }
-            if(grid[m-1][j]==1 && !visited[m-1][j]){
-                dfs(grid,visited,m-1,j);
+            if(grid[m-1][j]==1){
+                dfs(grid,m-1,j);
             }
         }
         int count=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==1 && visited[i][j]==false){
+                if(grid[i][j]==1){
                     count++;
                 }
             }
