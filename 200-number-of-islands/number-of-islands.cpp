39168285a070c9ff2edf566089340187af72037e
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2025-03-04 11:51:02 +0530

    Time: 24 ms (78.32%) | Memory: 16 MB (99.56%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,30 +1,50 @@
 class Solution {
 public:
+    void dfs(vector<vector<char>>& grid,int i,int j){
+        int m=grid.size();
+        int n=grid[0].size();
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]=='0'){
+            return;
+        }
+        grid[i][j]='0';
+        dfs(grid,i+1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j-1);
+    }
     int numIslands(vector<vector<char>>& grid) {
         int island_count=0;
         int m=grid.size();
         int n=grid[0].size();
-        vector<pair<int,int>> dirs={{1,0},{0,1},{-1,0},{0,-1}};
-        queue<pair<int,int>> q;
+        // vector<pair<int,int>> dirs={{1,0},{0,1},{-1,0},{0,-1}};
+        // queue<pair<int,int>> q;
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(grid[i][j]=='1'){
+        //             island_count++;
+        //             q.push({i,j});
+        //             while(!q.empty()){
+        //                 int a=q.front().first;
+        //                 int b=q.front().second;
+        //                 q.pop();
+        //                 grid[a][b]='0';
+        //                 for(auto [x,y]:dirs){
+        //                     int dx=a+x;
+        //                     int dy=b+y;
+        //                     if(dx>=0 && dx<m && dy>=0 && dy<n && grid[dx][dy]=='1'){
+        //                         grid[dx][dy]='0';
+        //                         q.push({dx,dy});
+        //                     }
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
                     island_count++;
-                    q.push({i,j});
-                    while(!q.empty()){
-                        int a=q.front().first;
-                        int b=q.front().second;
-                        q.pop();
-                        grid[a][b]='0';
-                        for(auto [x,y]:dirs){
-                            int dx=a+x;
-                            int dy=b+y;
-                            if(dx>=0 && dx<m && dy>=0 && dy<n && grid[dx][dy]=='1'){
-                                grid[dx][dy]='0';
-                                q.push({dx,dy});
-                            }
-                        }
-                    }
+                    dfs(grid,i,j);
                 }
             }
         }
