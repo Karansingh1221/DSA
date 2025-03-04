4af79a49bebce454f7f807e01f5553602bd08869
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2025-03-04 11:31:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (62.99%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -5,17 +5,11 @@ public:
         int m=word1.size();
         int n=word2.size();
         string res="";
-        int turn=0;
         while(left<m && right<n){
-            if(turn%2==0){
-                res+=word1[left];
-                left++;
-                turn++;
-            }else{
-                res+=word2[right];
-                right++;
-                turn++;
-            }
+            res+=word1[left];
+            res+=word2[right];
+            left++;
+            right++;
         }
         while(left<m){
             res+=word1[left];
