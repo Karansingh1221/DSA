78e9163389e2cc1bfb0c00edb4be36e4a1e4ad52
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2025-03-09 21:06:14 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (20.38%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -14,8 +14,31 @@ public:
         }
         for(int i=0;i<n;i++){
             ans+=min(left[i],right[i])-height[i];
-        }        
+        }
         return ans;
+
+
+
+
+        // int n=height.size();
+        // vector<int> left(n);
+        // vector<int> right(n);
+        // left[0]=height[0];
+        // right[n-1]=height[n-1];
+        // int j=n-1;
+        // int ans=0;
+        // for(int i=1;i<n;i++){
+        //     left[i]=max(left[i-1],height[i]);
+        //     right[j-i]=max(right[j-i+1],height[j-i]);
+        // }
+        // for(int i=0;i<n;i++){
+        //     ans+=min(left[i],right[i])-height[i];
+        // }        
+        // return ans;
+
+
+
+
 
 
 
@@ -34,5 +57,9 @@ public:
         //     ans+=min(left[i],right[i])-height[i];
         // }
         // return ans;
+
+
+
+        
     }
 };
