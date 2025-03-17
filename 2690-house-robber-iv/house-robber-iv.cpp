fe1eda12d6117b45a232b6a909130fcb63e075af
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2025-03-17 16:40:26 +0530

    Time: 15 ms (93.40%) | Memory: 60.7 MB (92.22%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.cpp
+++ 2690-house-robber-iv/house-robber-iv.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-    bool canRob(vector<int>& nums, int mid, int k) {
-        int count = 0, n = nums.size();
-        for (int i = 0; i < n; i++) {
-            if (nums[i] <= mid) {
+    bool check(vector<int>& nums,int mid,int k){
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<=mid){
                 count++;
                 i++;
             }
         }
-        return count >= k;
+        return count>=k;
     }
-
     int minCapability(vector<int>& nums, int k) {
-        int left = 1, right = *max_element(nums.begin(), nums.end()), ans = right;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (canRob(nums, mid, k)) {
-                ans = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+        int left=1;
+        int right=*max_element(nums.begin(),nums.end());
+        int n=nums.size();
+        int ans=0;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(check(nums,mid,k)){
+                ans=mid;
+                right=mid-1;
+            }else{
+                left=mid+1;
             }
-        }
+        }        
         return ans;
     }
 };
