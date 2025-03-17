2c2bd8e1e6165c834f18febf12914589649a6bc7
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2025-03-17 21:18:43 +0530

    Time: 80 ms (91.12%) | Memory: 61.9 MB (87.32%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
+        // int n=nums.size();
+        // unordered_map<int,int> mp;
+        // int count=0;
+        // for(int i=0;i<n;i++){
+        //     int diff=k-nums[i];
+        //     if(mp.find(diff)!=mp.end()){
+        //         count++;
+        //         mp[diff]--;
+        //         if(mp[diff]==0) mp.erase(diff);
+        //     }else{
+        //         mp[nums[i]]++;
+        //     }
+        // }
+        // return count;
+
+
+        sort(nums.begin(),nums.end());
         int n=nums.size();
-        unordered_map<int,int> mp;
-        int count=0;
-        for(int i=0;i<n;i++){
-            int diff=k-nums[i];
-            if(mp.find(diff)!=mp.end()){
+        int left=0;
+        int right=n-1,count=0;
+        while(left<right){
+            if((nums[left]+nums[right])==k){
                 count++;
-                mp[diff]--;
-                if(mp[diff]==0) mp.erase(diff);
+                left++;
+                right--;
+            }else if((nums[left]+nums[right])>k){
+                right--;
             }else{
-                mp[nums[i]]++;
+                left++;
             }
         }
         return count;
+
     }
 };
 
-
-//Two Pointer Approach using Sorting
-// sort(nums.begin(),nums.end());
-//         int n=nums.size();
-//         int left=0;
-//         int right=n-1,count=0;
-//         while(left<right){
-//             if((nums[left]+nums[right])==k){
-//                 count++;
-//                 left++;
-//                 right--;
-//             }else if((nums[left]+nums[right])>k){
-//                 right--;
-//             }else{
-//                 left++;
-//             }
-//         }
-//         return count;
