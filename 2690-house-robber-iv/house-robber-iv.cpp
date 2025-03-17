dafdb7caa3f68195c0fc861f4fc658f66860a917
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2025-03-17 16:41:00 +0530

    Time: 21 ms (60.99%) | Memory: 61 MB (30.44%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.cpp
+++ 2690-house-robber-iv/house-robber-iv.cpp
@@ -13,7 +13,6 @@ public:
     int minCapability(vector<int>& nums, int k) {
         int left=1;
         int right=*max_element(nums.begin(),nums.end());
-        int n=nums.size();
         int ans=0;
         while(left<=right){
             int mid=left+(right-left)/2;
