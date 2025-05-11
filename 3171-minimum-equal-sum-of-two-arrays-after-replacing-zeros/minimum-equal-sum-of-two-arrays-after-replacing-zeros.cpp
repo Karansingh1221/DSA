dd92fa89190da88bda7ac2258e68957944d40367
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2025-05-11 11:28:32 +0530

    Time: 132 ms (52.22%) | Memory: 135.4 MB (50.51%) - LeetSync

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     long long minSum(vector<int>& nums1, vector<int>& nums2) {
-        ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    long long z1 = count(nums1.begin(), nums1.end(), 0), 
-              z2 = count(nums2.begin(), nums2.end(), 0);
-    long long s1 = accumulate(nums1.begin(), nums1.end(), z1),
-              s2 = accumulate(nums2.begin(), nums2.end(), z2);
-
-    if ((s1 < s2 && !z1) || (s2 < s1 && !z2)) return -1;
-    return max(s1, s2);
+        long long c1=count(nums1.begin(),nums1.end(),0),
+                  c2=count(nums2.begin(),nums2.end(),0);
+        long long s1=accumulate(nums1.begin(),nums1.end(),c1),
+                  s2=accumulate(nums2.begin(),nums2.end(),c2);
+        if((s1<s2 && !c1) || (s2<s1 && !c2)) return -1;
+        return max(s1,s2);
     }
 };
