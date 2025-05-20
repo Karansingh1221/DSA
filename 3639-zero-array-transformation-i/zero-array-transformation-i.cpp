afa95c06c9602c65b5a60f51f84b7e58b75252c8
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2025-05-20 12:25:36 +0530

    Time: 143 ms (19.63%) | Memory: 325.3 MB (29.92%) - LeetSync

---
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    // void solve(vector<int>& nums,int left,int right){
+    //     for(int i=left;i<=right;i++){
+    //         if(nums[i]!=0){
+    //             nums[i]-=1;
+    //         }
+    //     }
+    // }
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        // for(int i=0;i<queries.size();i++){
+        //     solve(nums,queries[i][0],queries[i][1]);
+        // }
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]!=0){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        int n=nums.size();
+        int m=queries.size();
+        vector<int> cmp(n+1,0);
+        for(auto q:queries){
+            cmp[q[0]]++;
+            if(q[1]+1<n) cmp[q[1]+1]--;
+        }
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            cnt+=cmp[i];
+            if(nums[i]>cnt) return false;
+        }
+        return true;
+        
+    }
+};
