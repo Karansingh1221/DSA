cd48ee19fb2067cbf4047d7cd17c7f45d5597366
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2025-06-16 11:52:14 +0530

    Time: 0 ms (100.00%) | Memory: 77.2 MB (12.36%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -14,7 +14,7 @@ public:
         int carry=0;
         ListNode *dummy=new ListNode();
         ListNode *curr=dummy;
-        while(l1 || l2 || carry){
+        while(l1 || l2 ){
             int sum=carry;
             if(l1) {sum+=l1->val; l1=l1->next;}
             if(l2){ sum+=l2->val; l2=l2->next;}
