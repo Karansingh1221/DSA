a8081d3732b652b3c6705cbb289e390c675b980a
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2025-06-26 10:06:42 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (19.90%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,64 +2,20 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n=height.size();
-        vector<int> left(n);
-        vector<int> right(n);
-        left[0]=height[0];
-        right[n-1]=height[n-1];
-        int j=n-1;
-        int ans=0;
+        vector<int> l(n);
+        l[0]=height[0];
+        vector<int> r(n);
+        r[n-1]=height[n-1];
         for(int i=1;i<n;i++){
-            left[i]=max(left[i-1],height[i]);
-            right[j-i]=max(right[j-i+1],height[j-i]);
+            l[i]=max(l[i-1],height[i]);
         }
+        for(int i=n-2;i>=0;i--){
+            r[i]=max(r[i+1],height[i]);
+        }
+        int sum=0;
         for(int i=0;i<n;i++){
-            ans+=min(left[i],right[i])-height[i];
+            sum+=min(l[i],r[i])-height[i];
         }
-        return ans;
-
-
-
-
-        // int n=height.size();
-        // vector<int> left(n);
-        // vector<int> right(n);
-        // left[0]=height[0];
-        // right[n-1]=height[n-1];
-        // int j=n-1;
-        // int ans=0;
-        // for(int i=1;i<n;i++){
-        //     left[i]=max(left[i-1],height[i]);
-        //     right[j-i]=max(right[j-i+1],height[j-i]);
-        // }
-        // for(int i=0;i<n;i++){
-        //     ans+=min(left[i],right[i])-height[i];
-        // }        
-        // return ans;
-
-
-
-
-
-
-
-
-
-
-        // left[0]=height[0];
-        // right[n-1]=height[n-1];
-        // int j=n-1;
-        // for(int i=1;i<n;i++){
-        //     left[i]=max(left[i-1],height[i]);
-        //     right[j-i]=max(right[j-i+1],height[j-i]);
-        // }
-        // int ans=0;
-        // for(int i=0;i<n;i++){
-        //     ans+=min(left[i],right[i])-height[i];
-        // }
-        // return ans;
-
-
-
-        
+        return sum;
     }
 };
