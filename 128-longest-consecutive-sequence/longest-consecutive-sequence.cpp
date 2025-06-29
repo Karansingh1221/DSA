8a02ea539fece8994c3d15f4be41db1a0743357c
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2025-06-29 23:05:56 +0530

    Time: 79 ms (70.23%) | Memory: 88.9 MB (28.91%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -15,5 +15,25 @@ public:
         }
         return ans;
 
+
+
+        // for(int num:nums){
+        //     mp[num]++;
+        // }
+        // int count=1;
+        // vector<int> v;
+        // for(auto pair:mp){
+        //     v.push_back(pair.first);
+        // }
+        // int ans=1;
+        // for(int i=1;i<v.size();i++){
+        //     if((v[i]-v[i-1])==1){
+        //         count++;
+        //     }else{
+        //         count=1;
+        //     }
+        //     ans=max(ans,count);
+        // }
+        // return ans;
     }
 };
