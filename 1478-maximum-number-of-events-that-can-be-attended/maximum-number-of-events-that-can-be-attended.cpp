aa8f0c2258972d2cfc056d753bb1efe47474bfbe
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2025-07-07 10:14:06 +0530

    Time: 59 ms (79.86%) | Memory: 74.9 MB (64.96%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.cpp
@@ -1,51 +1,29 @@
 class Solution {
 public:
     int maxEvents(vector<vector<int>>& events) {
-        sort(events.begin(),events.end());
-        // int maxi=-1;
-        // for(int i=0;i<events.size();i++){
-        //     maxi=max(maxi,max(events[i][0],events[i][1]));
-        // }
-        // vector<int> visited(maxi+1,false);
-        // priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        // for(int i=0;i<events.size();i++){
-        //     pq.push({events[i][1],events[i][0]});
-        // }
-        // int count=0;
-        // while(!pq.empty()){
-        //     auto pair=pq.top();
-        //     pq.pop();
-        //     for(int i=pair.second;i<=pair.first;i++){
-        //         if(!visited[i]){
-        //             visited[i]=true;
-        //             count++;
-        //             break;
-        //         }
-        //     }
-        // }
-        // return count;
-
-
-        int n=events.size();
-        int day=0;
-        int count=0;
-        int index=0;
-        priority_queue<int,vector<int>,greater<int>> pq;
-        while(!pq.empty() || index<n){
-            if(pq.empty()){
-                day=events[index][0];
+        // Sort events based on start day
+        sort(events.begin(), events.end());
+        
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        
+        int day = 0, index = 0, n = events.size(), result = 0;
+        
+        while (!minHeap.empty() || index < n) {
+            if (minHeap.empty()) {
+                day = events[index][0];
             }
-            while(index<n && events[index][0]<=day){
-                pq.push(events[index][1]);
+            while (index < n && events[index][0] <= day) {
+                minHeap.push(events[index][1]);
                 index++;
             }
-            pq.pop();
+            minHeap.pop();
+            result++;
             day++;
-            count++;
-            while(!pq.empty() && pq.top()<day){
-                pq.pop();
+            
+            while (!minHeap.empty() && minHeap.top() < day) {
+                minHeap.pop();
             }
         }
-        return count;
+        return result;
     }
 };
