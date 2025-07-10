052e1e45d203adde61e4b03606dfc621a5609943
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2025-07-10 11:44:41 +0530

    Time: 0 ms (100.00%) | Memory: 112.3 MB (72.02%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,29 +1,53 @@
 class Solution {
 public:
-    bool check(vector<int>& diff,int i){
-        int n=diff.size();
-        int res=diff[i];
-        int temp=i+1;
-        while((temp%n)!=i){
-            if(res<0) return false;
-            res+=diff[temp%n];
-            temp++;
-        }
-        return true;
-    }
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int start=0;
         int n=gas.size();
-        vector<int> diff(n);
+        int tank=0;
+        int total_gas=0;
+        int total_cost=0;
         for(int i=0;i<n;i++){
-            diff[i]=gas[i]-cost[i];
-        }
-        int sum=accumulate(diff.begin(),diff.end(),0);
-        if(sum<0) return -1;
-        for(int i=0;i<n;i++){
-            if(diff[i]>0 && check(diff,i)){
-                return i;
+            total_gas+=gas[i];
+            total_cost+=cost[i];
+            tank+=gas[i]-cost[i];
+            if(tank<0) {
+                start=i+1;
+                tank=0;
             }
         }
-        return 0;
+        return (total_gas<total_cost)?-1:start;
     }
 };
+
+
+
+// Optimized Brute Force
+// class Solution {
+// public:
+//     bool check(vector<int>& diff,int i){
+//         int n=diff.size();
+//         int res=diff[i];
+//         int temp=i+1;
+//         while((temp%n)!=i){
+//             if(res<0) return false;
+//             res+=diff[temp%n];
+//             temp++;
+//         }
+//         return true;
+//     }
+//     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+//         int n=gas.size();
+//         vector<int> diff(n);
+//         for(int i=0;i<n;i++){
+//             diff[i]=gas[i]-cost[i];
+//         }
+//         int sum=accumulate(diff.begin(),diff.end(),0);
+//         if(sum<0) return -1;
+//         for(int i=0;i<n;i++){
+//             if(diff[i]>0 && check(diff,i)){
+//                 return i;
+//             }
+//         }
+//         return 0;
+//     }
+// };
