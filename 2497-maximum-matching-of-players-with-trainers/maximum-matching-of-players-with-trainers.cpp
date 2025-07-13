4c0d39921cf4d4085fc74291b58510bc9e4102ba
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2025-07-13 14:28:22 +0530

    Time: 32 ms (31.16%) | Memory: 80.3 MB (60.41%) - LeetSync

--- 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.cpp
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.cpp
@@ -2,25 +2,38 @@ class Solution {
 public:
     int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
         int count=0;
-        priority_queue<int> pq1;
-        priority_queue<int> pq2;
-        for(int num:players){
-            pq1.push(num);
-        }
-        for(int num:trainers){
-            pq2.push(num);
-        }
-        while(!pq1.empty() && !pq2.empty()){
-            if(pq1.top()<=pq2.top()){
-                pq1.pop();
-                pq2.pop();
+        sort(players.begin(),players.end());
+        sort(trainers.begin(),trainers.end());
+        int i=0;
+        int j=0;
+        while(i<players.size() && j<trainers.size()){
+            if(players[i]>trainers[j]) j++;
+            else if(players[i]<=trainers[j]){
+                i++;
+                j++;
                 count++;
             }
-            else if(pq1.top()>pq2.top()){
-                pq1.pop();
-            }
-        }
+        }        
         return count;
+        // priority_queue<int> pq1;
+        // priority_queue<int> pq2;
+        // for(int num:players){
+        //     pq1.push(num);
+        // }
+        // for(int num:trainers){
+        //     pq2.push(num);
+        // }
+        // while(!pq1.empty() && !pq2.empty()){
+        //     if(pq1.top()<=pq2.top()){
+        //         pq1.pop();
+        //         pq2.pop();
+        //         count++;
+        //     }
+        //     else if(pq1.top()>pq2.top()){
+        //         pq1.pop();
+        //     }
+        // }
+        // return count;
 
     }
 };
