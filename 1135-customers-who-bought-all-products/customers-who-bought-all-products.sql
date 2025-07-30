e55a4ccd51fd9a1b9e47fa1ae54904a185b77f5b
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2025-07-31 00:12:42 +0530

    Time: 587 ms (39.49%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,6 +1,14 @@
 # Write your MySQL query statement below
-select c.customer_id from Customer c
-where c.product_key is not NULL
--- join Product p on
-group by c.customer_id
-having count(distinct c.product_key)=(select count(p.product_key) from Product p);
+-- select c.customer_id from Customer c
+-- where c.product_key is not NULL
+-- group by c.customer_id
+-- having count(distinct c.product_key)=(select count(p.product_key) from Product p);
+
+
+select customer_id from
+(
+    select * from Customer
+    group by customer_id,product_key
+) a
+group by customer_id
+having count(*)=(select count(*) from Product);
