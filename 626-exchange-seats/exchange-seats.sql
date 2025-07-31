bb8513fd4eba9b44eaac6e34aa77059eec5ddc62
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2025-07-31 22:42:59 +0530

    Time: 396 ms (16.40%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,8 +1,17 @@
-select id,case
-    when id%2=1 and id!=(select max(id) from seat)
-        then lead(student) over (order by id) 
-    when id%2=0 
-        then lag(student) over (order by id)
-    else student 
-    end as student 
-from seat order by id;
+-- select id,case
+--     when id%2=1 and id!=(select max(id) from seat)
+--         then lead(student) over (order by id) 
+--     when id%2=0 
+--         then lag(student) over (order by id)
+--     else student 
+--     end as student 
+-- from seat order by id;
+
+select case
+when id%2=1 and id!=(select max(id) from seat) 
+then id+1
+when id%2=0
+then id-1
+else id
+end as id,student from seat
+order by id asc;
