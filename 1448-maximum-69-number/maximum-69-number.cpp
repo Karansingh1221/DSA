c587b567ffdf123ef7d3daaeb3efa671c5b468fa
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2025-08-17 00:07:57 +0530

    Time: 2 ms (2.59%) | Memory: 8.4 MB (12.18%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -1,11 +1,14 @@
-// C++
 class Solution {
 public:
     int maximum69Number (int num) {
-        std::string s = std::to_string(num);
-        for (char &c : s) {
-            if (c == '6') { c = '9'; break; }
+        string ans=to_string(num);
+        for(int i=0;i<ans.size();i++){
+            string temp=to_string(num);
+            temp[i]=temp[i]=='6' ? '9':'6';
+            if(stoi(temp)>stoi(ans)){
+                ans=temp;
+            }
         }
-        return std::stoi(s);
+        return stoi(ans);
     }
 };
