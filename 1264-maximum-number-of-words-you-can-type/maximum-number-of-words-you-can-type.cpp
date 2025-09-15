116ee53b7ec55f323482f596d4ff4224fe921105
Karan Singh <115416879+Karansingh1221@users.noreply.github.com>
2025-09-15 16:51:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (39.20%) - LeetSync

--- 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
@@ -1,30 +1,47 @@
 class Solution {
 public:
     int canBeTypedWords(string text, string letters) {
+        // int count=0;
+        // vector<string> words;
+        // string word="";
+        // for(char c:text){
+        //     if(c==' '){
+        //         if(word!=" "){
+        //             words.push_back(word);
+        //         }
+        //         word="";
+        //         continue;
+        //     }
+        //     word+=c;
+        // }
+        // if(word!=" "){
+        //     words.push_back(word);
+        // }
+        // for(auto w:words){
+        //     for(char c:w){
+        //         if(letters.find(c)!=string::npos){
+        //             count++;
+        //             break;
+        //         }
+        //     }
+        // }
+        // return words.size()-count;
+
         int count=0;
-        vector<string> words;
-        string word="";
-        for(char c:text){
-            if(c==' '){
-                if(word!=" "){
-                    words.push_back(word);
-                }
-                word="";
-                continue;
-            }
-            word+=c;
-        }
-        if(word!=" "){
-            words.push_back(word);
-        }
-        for(auto w:words){
-            for(char c:w){
+        istringstream iss(text);
+        string word;
+        while(iss >> word){
+            bool valid=true;
+            for(char c: word){
                 if(letters.find(c)!=string::npos){
-                    count++;
+                    valid=false;
                     break;
                 }
             }
+            if(valid){
+                count++;
+            }
         }
-        return words.size()-count;
+        return count;
     }
 };
